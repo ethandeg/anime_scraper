b6a04538cc332fe5ce3a378dc8f63ecc7ded6bae
Ethan Degenhardt <ethandeg1996@gmail.com>
2022-09-17 02:27:42 -0400

    parsing genre, studio, and some rankings

---
+++ anime_scraper/spiders/genre_spider.py
@@ -0,0 +1,46 @@
+import scrapy
+import os
+class GenreSpider(scrapy.Spider):
+    name="genre_spider"
+    start_urls=['https://myanimelist.net/anime.php']
+
+    def parse(self,response):
+        if os.path.exists('genre.txt'):
+            os.remove("genre.txt")
+        if os.path.exists('rankings.txt'):
+            os.remove("rankings.txt")
+        if os.path.exists('studios.txt'):
+            os.remove("studios.txt")
+        genres_container =response.css('.genre-link')[0:4]
+        genres = genres_container.css('.genre-name-link')
+        rankings=response.css('.genre-link')[5]
+        studios=response.css('.genre-link')[4]
+        for genre in genres:
+            # yield {'genre':genre.css('::text').get()}
+            g=genre.css('::text').get().split(' (')[0]
+            url=genre.css('::attr(href)').extract()[0]
+            g_id=url.split('/')[3]
+            with open("genre.txt", 'a') as f:
+                f.write(f"name: {g}, id: {g_id}\n")
+        f.close()
+        ranking_cats=rankings.css('.genre-name-link')
+        for rank in ranking_cats:
+            r=rank.css('::text').get().split(' (')[0]
+            with open("rankings.txt",'a') as f:
+                f.write(f"{r}\n")
+        f.close()
+        studs=studios.css('.genre-name-link')
+        for stud in studs:
+            s=stud.css('::text').get().split(' (')[0]
+            url=stud.css('::attr(href)').extract()[0]
+            id=url.split('/')[3]
+            with open("studios.txt",'a') as f:
+                f.write(f"name: {s}, id: {id}\n")
+        
+            
+        #     for anime in name:
+        #         a=anime.split(' (')[0]
+        #         print(a)
+        #         with open('genres.txt','a') as f:
+        #             f.write(f"{anime}\n")
+        # f.close()
