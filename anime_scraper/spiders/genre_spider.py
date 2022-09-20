b2cd0f833eecc7006594ce869b64d829bc0da0e7
Ethan Degenhardt <ethandeg1996@gmail.com>
2022-09-20 00:14:32 -0400

    got genres, rankings, and studios scraping, now onto anime

--- anime_scraper/spiders/genre_spider.py
+++ anime_scraper/spiders/genre_spider.py
@@ -25,23 +25,20 @@ class GenreSpider(scrapy.Spider):
             g=genre.css('::text').get().split(' (')[0]
             url=genre.css('::attr(href)').extract()[0]
             g_id=url.split('/')[3]
-            yield {'id': g_id,'name': g}
+            yield {'id': g_id,'name': g,'type': 'genre'}
         #     with open("genre.txt", 'a') as f:
         #         f.write(f"name: {g}, id: {g_id}\n")
         # f.close()
         ranking_cats=rankings.css('.genre-name-link')
-        # for rank in ranking_cats:
-        #     r=rank.css('::text').get().split(' (')[0]
-        #     with open("rankings.txt",'a') as f:
-        #         f.write(f"{r}\n")
-        # f.close()
-        # studs=studios.css('.genre-name-link')
-        # for stud in studs:
-        #     s=stud.css('::text').get().split(' (')[0]
-        #     url=stud.css('::attr(href)').extract()[0]
-        #     id=url.split('/')[3]
-        #     with open("studios.txt",'a') as f:
-        #         f.write(f"name: {s}, id: {id}\n")
+        for rank in ranking_cats:
+            r=rank.css('::text').get().split(' (')[0]
+            yield {'id': 1,'name': r, 'type': 'rank'}
+        studs=studios.css('.genre-name-link')
+        for stud in studs:
+            s=stud.css('::text').get().split(' (')[0]
+            url=stud.css('::attr(href)').extract()[0]
+            id=url.split('/')[3]
+            yield {'id': id, 'name': s, 'type': 'studio'}
         
             
         #     for anime in name:
